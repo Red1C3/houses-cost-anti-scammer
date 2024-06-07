d9cf709c8f1555eae7485bc98fec5bc0fca17bd7
Mohammad Issawi <2000issawi@gmail.com>
2024-06-07 19:41:10 +0300

    Half working test suite

--- test/test.py
+++ test/test.py
@@ -1,4 +1,24 @@
 import pandas as pd
+from tqdm import tqdm
+from model.model import Model
+from rules_makers.dummy import DummyRulesMaker
+
+tqdm.pandas()
+
+error=0
+
 def run():
+
+    model=Model(DummyRulesMaker(),10000)    
+
     data=pd.read_csv('kc_house_data.csv')
     
+    data=data[['sqft_living','sqft_lot','sqft_basement','view','bedrooms','bathrooms','floors','condition','lat','long','price']]
+
+    def se(x):
+        global error
+        error+=(model.predict(x[:-1].to_dict())-x[-1])**2
+    
+    data.progress_apply(se,axis=1)
+
+    return error/data.shape[0]
