03731e0db15f88f00bf311ac9964dd597b67bdfc
Mohammad Issawi <2000issawi@gmail.com>
2024-06-08 15:52:40 +0300

    Use ola's rules maker

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ import sys
 from model.model import Model
 import matplotlib.pyplot as plt
 from rules_makers.dummy import DummyRulesMaker
+from rules_makers.rule_maker import RulesMaker
 from test import test
+import warnings
+
+warnings.filterwarnings(action='ignore')
 
 def main():
     # Run test if test was given in CLI
@@ -16,7 +20,7 @@ def main():
         return
 
 
-    model=Model(DummyRulesMaker(),1e5)
+    model=Model(RulesMaker(),1e4)
     print(model.predict({'sqft_living':0,'sqft_lot':0,'sqft_basement':0,'view':0,'bedrooms':0,'bathrooms':0,'floors':1,'condition':1,'lat':47.5112,'long':-122.257}))
 
 if __name__=='__main__':
