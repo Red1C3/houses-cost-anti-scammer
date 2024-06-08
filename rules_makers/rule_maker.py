25d2ebe819086a9bff8e97faa032f0806d373372
Ola Murtada <olamurtadaa@gmail.com>
2024-06-08 15:29:57 +0300

    add rule maker

---
+++ rules_makers/rule_maker.py
@@ -0,0 +1,15 @@
+from skfuzzy import control as ctrl
+
+from . import and_rule
+
+class RulesMaker:
+    def get_rules(self,input_vars,output_var):
+        return [
+            ctrl.Rule(input_vars['amenities']['poor'] | input_vars['amenities']['acceptable'] ,output_var['cheap']),
+            ctrl.Rule((input_vars['distance']['far'] | input_vars['sqft_living']['med'])  & input_vars['sqft_living']['large'] ,output_var['affordable']),
+            ctrl.Rule(input_vars['sqft_living']['large'] & input_vars['distance']['far']  & (input_vars['amenities']['good'] | input_vars['amenities']['acceptable'])  ,output_var['affordable']),
+            ctrl.Rule(input_vars['sqft_living']['large'] & (input_vars['distance']['close'] | input_vars['distance']['med'] )  & (input_vars['amenities']['good'] | input_vars['amenities']['acceptable'])  ,output_var['expensive']  ),
+            ctrl.Rule(input_vars['amenities']['good'] ,output_var['expensive'])
+  
+
+        ]
