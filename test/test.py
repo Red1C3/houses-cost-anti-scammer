7926100b00f6d61c648352394122da58aecd0534
Mohammad Issawi <2000issawi@gmail.com>
2024-06-09 14:41:22 +0300

    Change testing from minmaxscaled to logarithmic error

--- test/test.py
+++ test/test.py
@@ -2,33 +2,29 @@ import pandas as pd
 from tqdm import tqdm
 from model.model import Model
 from rules_makers.dummy import DummyRulesMaker
-from sklearn.preprocessing import MinMaxScaler
+from rules_makers.rule_maker import RulesMaker
+from rules_makers.rule_maker_plus import RulesMakerPlus
 import numpy as np
+import math
 
 tqdm.pandas()
 
 error=0
 
 def run(samples_cap=None):
-
-    minmaxscaler=MinMaxScaler()
-
-
-    model=Model(DummyRulesMaker(),10000)    
+    model=Model(RulesMaker(),10000)    
 
     data=pd.read_csv('kc_house_data.csv')
-
-    minmaxscaler=minmaxscaler.fit([[75000.0],[7700000.0]])
 
     data=data[['sqft_living','sqft_lot','sqft_basement','view','bedrooms','bathrooms','floors','condition','lat','long','price']]
 
     def ae(x):
         global error
-        error+=abs( minmaxscaler.transform( np.array( model.predict(x[:-1].to_dict())).reshape(-1,1))[0][0]- minmaxscaler.transform( np.array([x[-1]]).reshape(-1,1))[0][0])
+        error+= (model.predict(x[:-1].to_dict())- x[-1])**2
     
     if samples_cap is not None:
         data=data[:samples_cap]
 
     data.progress_apply(ae,axis=1)
 
-    return error/data.shape[0]
+    return math.log( error/data.shape[0]+1)
