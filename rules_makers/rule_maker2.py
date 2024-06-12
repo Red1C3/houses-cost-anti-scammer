9af199f4191c7a35174ea6f8bcb12985411755b5
Mohammad Issawi <2000issawi@gmail.com>
2024-06-12 18:05:29 +0300

    Patch up rules

--- rules_makers/rule_maker2.py
+++ rules_makers/rule_maker2.py
@@ -10,7 +10,8 @@ class RulesMaker2:
             ctrl.Rule(input_vars['location']['acceptable'] & input_vars['amenities']['acceptable'] & (input_vars['size']['small'] | input_vars['size']['med']),output_var['affordable']),
             ctrl.Rule(input_vars['location']['acceptable'] & input_vars['amenities']['acceptable'] & input_vars['size']['large'],output_var['expensive']),
             ctrl.Rule(input_vars['location']['acceptable'] & input_vars['amenities']['good'],output_var['expensive']),
-            ctrl.Rule(input_vars['location']['good'] & input_vars['amenities']['poor'] & input_vars['size']['small'],output_var['cheap']),
+            ctrl.Rule(input_vars['location']['good'] & input_vars['amenities']['poor'],output_var['cheap']),
             ctrl.Rule(input_vars['location']['good'] & input_vars['amenities']['acceptable'] & (input_vars['size']['med'] | input_vars['size']['small']),output_var['affordable']),
+            ctrl.Rule(input_vars['location']['good'] & input_vars['amenities']['acceptable'] & input_vars['size']['large'],output_var['expensive']),
             ctrl.Rule(input_vars['location']['good'] & input_vars['amenities']['good'],output_var['expensive'])
         ]
