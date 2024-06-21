efc9737bddcafe688f75370f1aa05706924028e1
Ola Murtada <olamurtadaa@gmail.com>
2024-06-21 16:35:14 +0300

    Issawi rules with gui

--- rules_makers/rule_maker4.py
+++ rules_makers/rule_maker4.py
@@ -5,24 +5,24 @@ from . import and_rule
 class RulesMaker4:
     def get_rules(self,input_vars,output_var):
         return [
-                ctrl.Rule(input_vars['distance']['far'] & input_vars['amenities']['acceptable']& input_vars['size']['med'] ,output_var['affordable']),
-                ctrl.Rule(input_vars['distance']['far'] & input_vars['amenities']['acceptable']& input_vars['size']['small'] ,output_var['cheap']),
-                ctrl.Rule(input_vars['distance']['far'] & input_vars['amenities']['poor']& input_vars['size']['small'] ,output_var['cheap']),
-                ctrl.Rule(input_vars['distance']['far'] & input_vars['amenities']['good'] & input_vars['size']['med'],output_var['affordable']),
-                ctrl.Rule(input_vars['distance']['far'] & input_vars['amenities']['good'] & input_vars['size']['small'],output_var['affordable']),
+                ctrl.Rule(input_vars['distance']['far'],output_var['cheap']),
+                ctrl.Rule(input_vars['amenities']['poor'],output_var['cheap']),
+                ctrl.Rule(input_vars['size']['small'],output_var['cheap']),
 
                 ctrl.Rule(input_vars['distance']['far'] & input_vars['amenities']['acceptable'] & input_vars['size']['med'],output_var['affordable']),
-
-                ctrl.Rule(input_vars['distance']['close'] & input_vars['amenities']['acceptable'] & input_vars['size']['med'],output_var['expensive']),
-                ctrl.Rule(input_vars['distance']['close'] & input_vars['size']['med'],output_var['expensive']),
-
-                ctrl.Rule(input_vars['distance']['close'] & input_vars['amenities']['good'],output_var['expensive']),
-                ctrl.Rule(input_vars['distance']['close'] & input_vars['amenities']['poor'] & input_vars['size']['large'],output_var['expensive']),
-                ctrl.Rule(input_vars['distance']['close'] & input_vars['amenities']['acceptable'] & input_vars['size']['large'],output_var['expensive']),
-
-                ctrl.Rule(input_vars['distance']['med'] & input_vars['amenities']['good'] & input_vars['size']['large'],output_var['expensive']),
                 ctrl.Rule(input_vars['distance']['med'] & input_vars['amenities']['poor'] & input_vars['size']['med'],output_var['affordable']),
                 ctrl.Rule(input_vars['distance']['med'] & input_vars['amenities']['acceptable'] & input_vars['size']['small'],output_var['affordable']),
+
                 ctrl.Rule(input_vars['distance']['med'] & input_vars['amenities']['acceptable'] & input_vars['size']['med'],output_var['affordable']),
 
+                ctrl.Rule(input_vars['distance']['close'] & input_vars['amenities']['good'] & input_vars['size']['med'],output_var['expensive']),
+                ctrl.Rule(input_vars['distance']['close'] & input_vars['amenities']['acceptable'] & input_vars['size']['large'],output_var['expensive']),
+                ctrl.Rule(input_vars['distance']['med'] & input_vars['amenities']['good'] & input_vars['size']['large'],output_var['expensive']),
+
+
+                ctrl.Rule(input_vars['distance']['close'] & input_vars['amenities']['good'] & input_vars['size']['large'],output_var['expensive']),
+
+                ctrl.Rule(input_vars['distance']['close'],output_var['expensive']),
+                ctrl.Rule(input_vars['amenities']['good'],output_var['expensive']),
+                ctrl.Rule(input_vars['size']['large'],output_var['expensive']),
             ]
